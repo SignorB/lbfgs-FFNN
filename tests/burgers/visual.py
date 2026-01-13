e44fb509e3e588a6313727e503469c3b22516d1c
gbarocco <giorgio.barocco@mail.polimi.it>
2026-01-13 21:30:44 +0100

    updated visualization function to save png instead of showing

--- tests/burgers/visual.py
+++ tests/burgers/visual.py
@@ -4,29 +4,30 @@ import seaborn as sns
 import numpy as np
 from scipy.integrate import quad
 
-
-
 target_times = [0.0, 0.5, 1.2, 1.4]
 colors = ['tab:blue', 'tab:green', 'tab:red', 'tab:orange']
 nu = 0.3 / np.pi 
-
-
 
 def exact_solution(x, t):
     if t == 0:
         return np.sin(np.pi * x)
     
-
+    # Formula per la trasformata di Cole-Hopf
     def f(eta):
-        return np.exp(np.cos(np.pi * (x - eta)) / (2 * np.pi * nu * t)) * np.exp(-eta**2 / (4 * nu * t))
+        return np.exp(-np.cos(np.pi * (x - eta)) / (2 * np.pi * nu * t)) * np.exp(-eta**2 / (4 * nu * t))
     
     def g(eta):
-        return eta * f(eta)
+        return eta * f(eta) 
     
-    num, _ = quad(g, -np.inf, np.inf)
-    den, _ = quad(f, -np.inf, np.inf)
-    return num / den if den != 0 else 0
+    def f_user(eta):
+        return np.exp(np.cos(np.pi * (x - eta)) / (2 * np.pi * nu * t)) * np.exp(-eta**2 / (4 * nu * t))
+    
+    def g_user(eta):
+        return eta * f_user(eta)
 
+    num, _ = quad(g_user, -np.inf, np.inf)
+    den, _ = quad(f_user, -np.inf, np.inf)
+    return num / den if den != 0 else 0
 
 try:
     df = pd.read_csv('burgers_test_extrapolation.csv')
@@ -60,4 +61,8 @@ plt.xlabel("x")
 plt.ylabel("u(x,t)")
 plt.legend()
 plt.tight_layout()
-plt.show()
+
+output_filename = 'burgers_plot.png'
+plt.savefig(output_filename, dpi=300, bbox_inches='tight')
+print(f"Immagine salvata correttamente in: {output_filename}")
+plt.close()
