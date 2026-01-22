21d818f24c08ebb80e75c2c370535bf508740ee5
gbarocco <giorgio.barocco@mail.polimi.it>
2026-01-22 14:35:51 +0100

    added implementation for pinn with openmp

--- tests/burgers/test_burgers_parallel.cpp
+++ tests/burgers/test_burgers_parallel.cpp
@@ -1,25 +1,28 @@
-// PARALLEL (NOT OPTIMAL) VERSION, 1.5X on 4 cores  
-
 #include "../../src/enzyme/pinn_network.hpp"
 #include "../../src/minimizer/lbfgs.hpp"
 #include <iomanip>
 #include <vector>
 #include <cmath>
 #include <fstream>
+#include <memory>
 #include <Eigen/Dense>
 #include <omp.h> 
+#include <iostream>
+
+using namespace cpu_mlp;
 
 using VectorXr = Eigen::Matrix<Real, Eigen::Dynamic, 1>;
 using MatrixXr = Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic>;
 
 extern "C" {
     extern Real __enzyme_autodiff(void*, ...);
+    extern Real __enzyme_fwddiff(void*, ...); 
     int enzyme_dup;
     int enzyme_const;
     int enzyme_out;
 }
 
-constexpr unsigned int net_size = 100;
+constexpr unsigned int net_size = 50;
 using MyPINN = PINN<
     Dense<2, net_size, Tanh>,
     Dense<net_size, net_size, Tanh>,
@@ -27,182 +30,209 @@ using MyPINN = PINN<
     Dense<net_size, 1, Linear> 
 >;
 
-struct Point { Real x, t; };
+struct Point { Real x, t; Real target; };
 struct Data { 
     std::vector<Point> collocation; 
     std::vector<Point> initial;       
     std::vector<Point> boundary;    
 };
 
-Real diff_input(const Real* xt_ptr, const Real* p, int index) {
-    Real input_grad[2];
-    input_grad[0] = 0.0;
-    input_grad[1] = 0.0;
-    
-    __enzyme_autodiff((void*)MyPINN::forward_static, 
-                      enzyme_dup, xt_ptr, input_grad, 
-                      enzyme_const, p);
-    return input_grad[index];
+
+
+static Real get_u(const Real* xt, const Real* p) {
+    return MyPINN::forward_static(xt, p);
 }
 
-
-Real calc_ux(const Real* xt_ptr, const Real* p) {
-    return diff_input(xt_ptr, p, 0);
+static Real calc_du(const Real* xt, const Real* p, int index) {
+    Real d_xt[2] = {0.0, 0.0};
+    d_xt[index] = 1.0; 
+    return __enzyme_fwddiff((void*)get_u, enzyme_dup, xt, d_xt, enzyme_const, p);
 }
 
-Real pde_residual(Real x, Real t, const Real* p) {
+static Real get_ux(const Real* xt, const Real* p) {
+    return calc_du(xt, p, 0); 
+}
 
-    Real xt[2]; 
-    xt[0] = x; 
-    xt[1] = t;
+static Real pde_residual(Real x, Real t, const Real* p) {
+    Real xt[2] = {x, t};
+    Real nu = 0.01 / M_PI;
 
-    Real nu = 0.3 / M_PI; 
+    Real u = get_u(xt, p);
+    Real ut = calc_du(xt, p, 1);
+    Real ux = calc_du(xt, p, 0);
 
-    Real u = MyPINN::forward_static(xt, p);
-    Real ut = diff_input(xt, p, 1);
-    Real ux = diff_input(xt, p, 0);
-
-
-    Real uxx = 0.0;
-    Real grad_ux[2];
-    grad_ux[0] = 0.0; 
-    grad_ux[1] = 0.0;
-
-    __enzyme_autodiff((void*)calc_ux, 
-                      enzyme_dup, xt, grad_ux, 
-                      enzyme_const, p);
-    uxx = grad_ux[0];
+    Real d_xt_xx[2] = {1.0, 0.0};
+    Real uxx = __enzyme_fwddiff((void*)get_ux, enzyme_dup, xt, d_xt_xx, enzyme_const, p);
 
     return ut + u * ux - nu * uxx;
 }
 
-Real loss_function(Real* p, Data* data) {
-    Real loss_ic_sum = 0.0;
-    Real loss_bc_sum = 0.0;
-    Real loss_pde_sum = 0.0;
-    
+static Real loss_ic_single(const Real* p, const Real* xt_val, Real target) {
+    Real u = get_u(xt_val, p);
+    Real diff = u - target;
+    return diff * diff;
+}
 
-    int N_ic = (int)data->initial.size();
-    int N_bc = (int)data->boundary.size();
-    int N_pde = (int)data->collocation.size();
+static Real loss_bc_single(const Real* p, const Real* xt_val) {
+    Real u = get_u(xt_val, p);
+    return u * u;
+}
 
+static Real loss_pde_single(const Real* p, const Real* xt_val) {
+    Real r = pde_residual(xt_val[0], xt_val[1], p);
+    return r * r;
+}
 
-    #pragma omp parallel for
-    for (int i = 0; i < N_ic; ++i) {
-        Real x = data->initial[i].x;
-        Real xt[2]; xt[0] = x; xt[1] = 0.0;
+static Real weighted_loss_ic(const Real* p, const Real* xt, Real target, Real weight) {
+    return weight * loss_ic_single(p, xt, target);
+}
 
-        Real u = MyPINN::forward_static(xt, p); 
-        Real target = std::sin(M_PI * x);
-        Real diff = u - target;
-        Real sq_diff = diff * diff;
+static Real weighted_loss_bc(const Real* p, const Real* xt, Real weight) {
+    return weight * loss_bc_single(p, xt);
+}
 
-        #pragma omp atomic
-        loss_ic_sum += sq_diff;
-    }
-    Real loss_ic = (N_ic > 0) ? (loss_ic_sum / N_ic) : 0.0;
-
-
-    #pragma omp parallel for
-    for (int i = 0; i < N_bc; ++i) {
-        Real x = data->boundary[i].x;
-        Real t = data->boundary[i].t;
-        Real xt[2]; xt[0] = x; xt[1] = t;
-
-        Real u = MyPINN::forward_static(xt, p);
-        Real sq_u = u * u;
-
-        #pragma omp atomic
-        loss_bc_sum += sq_u;
-    }
-    Real loss_bc = (N_bc > 0) ? (loss_bc_sum / N_bc) : 0.0;
-    
-
-    #pragma omp parallel for
-    for (int i = 0; i < N_pde; ++i) {
-        Real x = data->collocation[i].x;
-        Real t = data->collocation[i].t;
-        
-        Real r = pde_residual(x, t, p);
-        Real sq_r = r * r;
-
-        #pragma omp atomic
-        loss_pde_sum += sq_r;
-    }
-    Real loss_pde = (N_pde > 0) ? (loss_pde_sum / N_pde) : 0.0;
-    
-
-    const Real w_ic = 1.0;
-    const Real w_bc = 2.0;
-    const Real w_pde = 4.0;
-
-    return w_bc * loss_bc + w_ic * loss_ic + w_pde * loss_pde;
+static Real weighted_loss_pde(const Real* p, const Real* xt, Real weight) {
+    return weight * loss_pde_single(p, xt);
 }
 
 int main() {
-    std::cout << "=== Burgers PINN con Enzyme & OpenMP Atomic ===\n";
+    std::cout << "=== Burgers PINN Parallel (Manual Gradient Accumulation) ===\n";
+    std::cout << "Threads: " << omp_get_max_threads() << "\n";
+
     MyPINN net;
     Data data;
 
     Real dx = 0.001;
     Real dt = 0.005;
-    Real int_dx = 0.03;
-    Real int_dt = 0.06;
+    Real int_dx = 0.01; 
+    Real int_dt = 0.02;
     Real X = 1.0;
     Real T = 1.0;
     
-    for(Real x=-1; x<=X; x+=dx) data.initial.push_back({x, 0.0});
+    for(Real x=-1; x<=X; x+=dx) {
+        Real target = std::sin(M_PI * x);
+        data.initial.push_back({x, 0.0, target});
+    }
     
     for(Real t=0; t<=T; t+=dt) {
-        data.boundary.push_back({-1.0, t});
-        data.boundary.push_back({1.0, t});
+        data.boundary.push_back({-1.0, t, 0.0});
+        data.boundary.push_back({1.0, t, 0.0});
     }
 
-    data.collocation.reserve(2000); 
     for(Real x=-1; x<=X; x+=int_dx)
         for(Real t=0; t<=1; t+=int_dt)
-            data.collocation.push_back({x, t});
+            data.collocation.push_back({x, t, 0.0});
+
+    std::cout << "PDE Points: " << data.collocation.size() << "\n";
 
     auto solver = std::make_shared<LBFGS<VectorXr, MatrixXr>>();
+    solver->setHistorySize(100);    
+    solver->setMaxLineIters(100);   
+    solver->setArmijoMaxIter(50);   
+    solver->setMaxIterations(1000); 
 
-    solver->setHistorySize(128);    
-    solver->setMaxLineIters(150);   
-    solver->setArmijoMaxIter(80);   
-
-    solver->setMaxIterations(6);
-
-    
     VectorXr w = Eigen::Map<VectorXr>(net.params.data(), MyPINN::TotalParams);
-    
 
-    w = solver->solve_with_enzyme<loss_function>(w, &data);
+    const Real w_ic = 20.0;
+    const Real w_bc = 20.0;
+    const Real w_pde = 1.0;
 
-    std::cout << "outputing file...\n";
-    std::ofstream outFile("burgers_test_extrapolation.csv");
-    outFile << "x,t,u,type\n"; 
-    
-    auto write_points = [&](Real t_start, Real t_end, Real t_step, 
-                            Real x_start, Real x_end, Real x_step, int type) {
-        for (Real t = t_start; t <= t_end; t += t_step) {
-            for (Real x = x_start; x <= x_end; x += x_step) {
-                Real xt[2]; xt[0] = x; xt[1] = t;
-                Real u = MyPINN::forward_static(xt, w.data());
-                outFile << x << "," << t << "," << u << "," << type << "\n";
+    VecFun<VectorXr, double> f_loss = [&](const VectorXr& params) -> double {
+        const Real* p_ptr = params.data();
+        double total_ic = 0.0, total_bc = 0.0, total_pde = 0.0;
+
+        #pragma omp parallel reduction(+:total_ic, total_bc, total_pde)
+        {
+            #pragma omp for nowait
+            for(size_t i=0; i<data.initial.size(); ++i) {
+                Real xt[2] = {data.initial[i].x, data.initial[i].t};
+                total_ic += loss_ic_single(p_ptr, xt, data.initial[i].target);
+            }
+
+            #pragma omp for nowait
+            for(size_t i=0; i<data.boundary.size(); ++i) {
+                Real xt[2] = {data.boundary[i].x, data.boundary[i].t};
+                total_bc += loss_bc_single(p_ptr, xt);
+            }
+
+            #pragma omp for
+            for(size_t i=0; i<data.collocation.size(); ++i) {
+                Real xt[2] = {data.collocation[i].x, data.collocation[i].t};
+                total_pde += loss_pde_single(p_ptr, xt);
             }
         }
+
+        if(!data.initial.empty()) total_ic /= data.initial.size();
+        if(!data.boundary.empty()) total_bc /= data.boundary.size();
+        if(!data.collocation.empty()) total_pde /= data.collocation.size();
+
+        return w_ic * total_ic + w_bc * total_bc + w_pde * total_pde;
     };
 
-    for (Real t : {0.0f, 0.5f, 1.0f}) {
-        for (Real x = -1.0; x <= 1.0; x += 0.02) {
-             Real xt[2]; xt[0] = x; xt[1] = t;
-             Real u = MyPINN::forward_static(xt, w.data());
-             outFile << x << "," << t << "," << u << ",0\n";
-        }
-    }
-    write_points(0.0, 1.0, 0.2, -1.5, -1.5, 1.0, 1);
-    write_points(0.0, 1.0, 0.2, 1.5, 1.5, 1.0, 1);
-    write_points(1.1, 1.5, 0.1, -1.0, 1.0, 0.05, 2);
+    GradFun<VectorXr> grad_loss_real = [&](const VectorXr& params) -> VectorXr {
+        VectorXr global_grad = VectorXr::Zero(params.size());
+        const Real* p_ptr = params.data();
+        
+        Real s_ic = w_ic / std::max(1.0, (double)data.initial.size());
+        Real s_bc = w_bc / std::max(1.0, (double)data.boundary.size());
+        Real s_pde = w_pde / std::max(1.0, (double)data.collocation.size());
 
+        #pragma omp parallel
+        {
+            VectorXr local_grad = VectorXr::Zero(params.size());
+            
+            #pragma omp for nowait
+            for(size_t i=0; i<data.initial.size(); ++i) {
+                Real xt[2] = {data.initial[i].x, data.initial[i].t};
+                __enzyme_autodiff((void*)weighted_loss_ic, 
+                                  enzyme_dup, p_ptr, local_grad.data(), 
+                                  enzyme_const, xt, 
+                                  enzyme_const, data.initial[i].target, 
+                                  enzyme_const, s_ic);
+            }
+
+            #pragma omp for nowait
+            for(size_t i=0; i<data.boundary.size(); ++i) {
+                Real xt[2] = {data.boundary[i].x, data.boundary[i].t};
+                __enzyme_autodiff((void*)weighted_loss_bc, 
+                                  enzyme_dup, p_ptr, local_grad.data(), 
+                                  enzyme_const, xt, 
+                                  enzyme_const, s_bc);
+            }
+
+            #pragma omp for
+            for(size_t i=0; i<data.collocation.size(); ++i) {
+                Real xt[2] = {data.collocation[i].x, data.collocation[i].t};
+                __enzyme_autodiff((void*)weighted_loss_pde, 
+                                  enzyme_dup, p_ptr, local_grad.data(), 
+                                  enzyme_const, xt, 
+                                  enzyme_const, s_pde);
+            }
+            
+            #pragma omp critical
+            {
+                global_grad += local_grad;
+            }
+        }
+        return global_grad;
+    };
+    auto start = std::chrono::high_resolution_clock::now();
+    w = solver->solve(w, f_loss, grad_loss_real);
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Training Time: " << elapsed.count() << "s\n";
+
+    std::ofstream outFile("burgers_test_extrapolation.csv");
+    outFile << "x,t,u,type\n"; 
+    auto write_grid = [&](Real t, int type) {
+        for (Real x = -1.0; x <= 1.0; x += 0.02) {
+             Real xt[2] = {x, t};
+             Real u = get_u(xt, w.data());
+             outFile << x << "," << t << "," << u << "," << type << "\n";
+        }
+    };
+    write_grid(0.0, 0); write_grid(0.5, 0); write_grid(1.0, 0); write_grid(1.5, 2);
     outFile.close();
     
     return 0;
--- tests/burgers/visual.py
+++ tests/burgers/visual.py
@@ -1,25 +1,26 @@
+import matplotlib
+matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-target_times = [0.0, 0.5, 1.2, 1.4]
-colors = ['tab:blue', 'tab:green', 'tab:red', 'tab:orange']
-nu = 0.03 / np.pi 
+target_times = [0.0, 0.5, 1.0, 1.5]
+colors = ['tab:blue', 'tab:green', 'tab:orange', 'tab:red']
+nu = 0.01 / np.pi 
 
-# sampling point on x
-nx = 100
+nx = 2000 
 x = np.linspace(-1, 1, nx)
 dx = x[1] - x[0]
-u = np.sin(np.pi * x) # initial condition
-
+u = -np.sin(np.pi * x) 
+u = np.sin(np.pi * x)  
 
 sigma = 0.5
 dt_diff = dx**2 / (2 * nu)
-dt_adv = dx / 1.0 
+dt_adv = dx / 1.0 # assumendo max|u| ~ 1
 dt = sigma * min(dt_diff, dt_adv)
 
-print(f"Simulating (nu={nu:.4f})...")
+print(f"Simulating Reference Solution (nu={nu:.4f}, dx={dx:.5f})...")
 
 history_u = {} 
 history_u[0.0] = u.copy()
@@ -31,56 +32,58 @@ while current_t < t_max:
     u_prev = u.copy()
     
     u_xx = (np.roll(u_prev, -1) - 2*u_prev + np.roll(u_prev, 1)) / dx**2
-
     u_x = (np.roll(u_prev, -1) - np.roll(u_prev, 1)) / (2*dx)
-    
-
     u = u_prev + dt * (-u_prev * u_x + nu * u_xx)
-    
     u[0] = 0.0
     u[-1] = 0.0
-    
     current_t += dt
     
     for t_targ in target_times:
-        if t_targ > 0 and abs(current_t - t_targ) < dt/2:
-            history_u[t_targ] = u.copy()
+        if t_targ > 0 and abs(current_t - t_targ) < dt:
+            if t_targ not in history_u:
+                history_u[t_targ] = u.copy()
+                print(f"Snapshot t={t_targ} calcolato.")
 
-print("Simulazione completata.")
+print("Simulazione numerica completata.")
 
+filename = 'burgers_test_extrapolation.csv'
 try:
-    df = pd.read_csv('burgers_test_extrapolation.csv')
+    df = pd.read_csv(filename)
+    print(f"Dati caricati da {filename}")
 except FileNotFoundError:
-    print("ERRORE: File 'burgers_test_extrapolation.csv' non trovato.")
+    print(f"ERRORE: File '{filename}' non trovato.")
+    print("Esegui prima ./test_burgers_parallel per generare il CSV.")
     exit()
+
 
 plt.figure(figsize=(12, 8))
 sns.set_theme(style="whitegrid")
 
 for i, t in enumerate(target_times):
     col = colors[i]
-    
 
     if t in history_u:
-        plt.plot(x, history_u[t], color=col, linestyle='-', linewidth=2, 
-                 label=f'Exact t={t}')
-    
-    
+        plt.plot(x, history_u[t], color=col, linestyle='-', linewidth=2, alpha=0.6,
+                 label=f'Exact (FDM) t={t}')
     available_t = df['t'].unique()
-    nearest_t = available_t[np.argmin(np.abs(available_t - t))]
+    if len(available_t) > 0:
+        nearest_t = available_t[np.argmin(np.abs(available_t - t))]
+        if abs(nearest_t - t) < 0.05:
+            subset = df[df['t'] == nearest_t].sort_values('x')
+            label_suffix = " (Train)" if t <= 1.0 else " (EXTRAP)"
+            marker_style = 'o' if t <= 1.0 else 'X' 
+            plt.scatter(subset['x'], subset['u'], color=col, marker=marker_style, s=50, 
+                        edgecolor='black', linewidth=0.5, zorder=10,
+                        label=f'PINN t={nearest_t:.1f}{label_suffix}')
 
-    subset = df[df['t'] == nearest_t].sort_values('x')
-    
-    label_suffix = " (Train)" if t <= 1.0 else " (Extrap)"
-    marker = 'o' 
-    
-    plt.scatter(subset['x'], subset['u'], color=col, marker=marker, s=40, alpha=0.7, 
-                edgecolor='black', linewidth=0.5,
-                label=f'PINN t={nearest_t:.1f}{label_suffix}')
-
-plt.title(f"Burgers' Equation: PINN vs Numerical Exact (nu={nu:.4f})", fontsize=16)
+plt.title(f"Burgers' Equation: Enzyme PINN vs Reference (nu={nu:.4f})", fontsize=16)
 plt.xlabel("x")
 plt.ylabel("u(x,t)")
+plt.ylim(-1.2, 1.2)
 plt.legend()
 plt.tight_layout()
-plt.show()
+
+output_img = "burgers_comparison.png"
+plt.savefig(output_img, dpi=300)
+print(f"Grafico salvato correttamente in: {output_img}")
+plt.close()
